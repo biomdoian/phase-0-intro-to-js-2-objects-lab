5bf2196a988a404094d6ec7aa0ebe1b97492927a
Ian <iankruze60@gmail.com>
2025-03-09 22:27:16 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+let employee = {}; 
+
+function updateEmployeeWithKeyAndValue(employeeToUpdate, key, value) {
+  return { ...employeeToUpdate, [key]: value };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeToUpdate, key, value) {
+  employeeToUpdate[key] = value;
+  return employeeToUpdate;
+}
+
+function deleteFromEmployeeByKey(employeeToUpdate, key) {
+  const { [key]: deletedKey, ...rest } = employeeToUpdate;
+  return rest;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeToUpdate, key) {
+  delete employeeToUpdate[key];
+  return employeeToUpdate;
+}
